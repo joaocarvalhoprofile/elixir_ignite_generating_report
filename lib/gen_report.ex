6b59c60f59d7312bcc37937d653c776e698a1ee8
Joao Carvalho <joaokvalho@gmail.com>
2021-08-19 20:24:32 -0300

    Add parser.ex

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,20 @@
 defmodule GenReport do
+  @meses %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
 
-  def build do
-    # TO DO
+  def build(number) do
+    @meses[number]
   end
 end
